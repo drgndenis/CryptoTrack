67e60da3dd28e085720666cd37a48119b1194ee1
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-11-30 13:02:49 +0300

    Initial Commit

---
+++ CryptoTrack/CryptoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackApp.swift
+//  CryptoTrack
+//
+//  Created by Denis DRAGAN on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
