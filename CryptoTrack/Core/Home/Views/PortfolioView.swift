9df64047aef3748a635016dde7b7505879ea520d
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-12-26 17:18:01 +0300

    Added input section in portfolioView and save button

--- CryptoTrack/Core/Home/Views/PortfolioView.swift
+++ CryptoTrack/Core/Home/Views/PortfolioView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct PortfolioView: View {
+    @Environment(\.dismiss) var dismiss
     @EnvironmentObject private var vm: HomeViewModel
     @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText = ""
     
     var body: some View {
         NavigationStack {
@@ -17,12 +19,24 @@ struct PortfolioView: View {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
                     coinLogoList
+                    
+                    if selectedCoin != nil {
+                        portfolioInputSection
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
             .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    topBarLeading
+                }
                 ToolbarItem(placement: .topBarTrailing) {
                     XMarkButton()
+                }
+            }
+            .onChange(of: vm.searchText) { value in
+                if value == "" {
+                    removeSelectedCoin()
                 }
             }
         }
@@ -51,12 +65,83 @@ extension PortfolioView {
                         .background(
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear
-                                    ,lineWidth: 1)
+                                        ,lineWidth: 1)
                         )
                 }
             }
+            .frame(height: 120)
             .padding()
         }
         .scrollIndicators(.hidden)
+    }
+    
+    private var portfolioInputSection: some View {
+        VStack {
+            HStack {
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                Spacer()
+                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimal() ?? "")
+            }
+            Divider()
+            HStack {
+                Text("Amount holding:")
+                Spacer()
+                TextField("Ex: 1.4", text: $quantityText)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+            }
+            Divider()
+            HStack {
+                Text("Current Value:")
+                Spacer()
+                Text(getCurrentValue().asCurrencyWith6Decimal())
+            }
+        }
+        .animation(.none)
+        .font(.headline)
+        .padding()
+    }
+    
+    private var topBarLeading: some View {
+        Button {
+            saveButtonPressed()
+        } label: {
+            Text("Save".uppercased())
+                .foregroundStyle(Color.theme.accent)
+                .font(.headline)
+        }
+        .opacity(selectedCoin != nil && selectedCoin?.currentHoldings != Double(quantityText) ? 1.0 : 0.0)
+    }
+    
+    private func saveButtonPressed() {
+        guard let coin = selectedCoin else { return }
+        
+        // Save  portfolio
+        
+        
+        
+        withAnimation(.easeIn) {
+            removeSelectedCoin()
+        }
+        
+        // Hide keyboard
+        UIApplication.shared.endEditing()
+        
+        // Hide sheet
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            dismiss()
+        }
+    }
+    
+    private func removeSelectedCoin() {
+        selectedCoin = nil
+        vm.searchText = ""
+    }
+    
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
     }
 }
--- CryptoTrack/CryptoTrackApp.swift
+++ CryptoTrack/CryptoTrackApp.swift
@@ -11,6 +11,12 @@ import SwiftUI
 struct CryptoTrackApp: App {
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        // Customize the appearance of navigation bar titles when the app is launched
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
